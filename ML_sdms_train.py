1e72a927d1ae2d0efd69cffbb37445d66dad5ad6
Daniel Furman <dryanfurman@gmail.com>
2020-10-30 15:06:01 -0700

    Add files via upload

---
+++ ML_sdms_train.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 15 11:26:03 2020
+
+@author: danielfurman
+
+"""
+
+# Binary classification with ten BioClim features. Five are a function of 
+# precipitation and five of temperature, all decorrelated below a 0.5
+# correlation threshold. We use PyCaret to train and tune (10-fold cv) our
+# models from a  train set that contains 80%  of the total data. We find that
+# random forest, xgboost, lgbm, catboost, and extra trees algorithms perform
+# best. The last step was to create blended model from these five.
+
+import pandas as pd
+from pycaret.utils import version
+version()
+from pycaret.classification import *
+
+data = pd.read_csv("/Users/danielfurman/Data_science_code/xantusia-data-main/xant-pycaret.csv")
+data = data.sample(frac=1)
+data = data.drop(['Unnamed: 0','Unnamed: 0.1'], axis = 1)
+exp_clf = setup(data, target = 'pa')
+compare_models()
+
+# create models
+etrees = create_model('et')
+xgboost = create_model('xgboost')
+catboost = create_model('catboost') 
+rf = create_model('rf') 
+lgbm = create_model('lightgbm')
+
+# save models as .pkl files
+finalize_model(etrees)
+save_model(etrees, 'xant_etrees')
+
+finalize_model(xgboost)
+save_model(tuned_xgboost, 'xant_xgb')
+
+finalize_model(catboost)
+save_model(catboost, 'xant_cboost')
+
+finalize_model(rf)
+save_model(tuned_rf, 'xant_rf')
+
+finalize_model(lgbm)
+save_model(lgbm, 'xant_lgbm')
+
+blender_specific = blend_models(estimator_list = [rf, lgbm],
+                method = 'soft')
+finalize_model(blender_specific)
+save_model(blender_specific, 'xant_blended')
+
+print('PyCaret training ended \n\n')
