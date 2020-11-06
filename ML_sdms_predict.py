96cc58a6b30605b1c6fae667c0ae3f8670fbd2fc
Daniel Furman <dryanfurman@gmail.com>
2020-11-06 07:56:30 -0800

    flake8 style updates

--- ML_sdms_predict.py
+++ ML_sdms_predict.py
@@ -8,18 +8,17 @@ Created on Thu Oct 15 11:26:05 2020
 
 # Now that we have trained our ML classifiers, we are ready to deploy the .pkl
 # files we saved from PyCaret's training and examine the model predictions
-# on the validation set and examine the models' performance. We first print 
+# on the validation set and examine the models' performance. We first print
 # validation set accuracy, as well as the F statistic and the 2x2 confusion
 # matrix. Finally, we visualize the AUC statistic with a ROC curve for each
-# model. We also examine the results of a blended model constructed from the 
+# model. We also examine the results of a blended model constructed from the
 # aforementioned five most predictive learners.
 
 from sklearn.metrics import confusion_matrix
 from matplotlib import pyplot as plt
 from matplotlib import style
 from sklearn.metrics import roc_curve, auc, f1_score
-from sklearn.neural_network import MLPClassifier
-from pycaret.classification import *
+from pycaret.classification import load_model
 import warnings
 import numpy as np
 import pandas as pd
@@ -35,32 +34,29 @@ CLASS_MAP = {
     'LGBoost Machine': ('-.', load_model('xant_lgbm')[23]),
     'Extra Trees': ('-.', load_model('xant_etrees')[23]),
     'XGBoost': ('-.', load_model('xant_xgb')[23]),
-    'Logistic Regression' : ('-.', load_model('xant_log')[23]),
+    'Logistic Regression': ('-.', load_model('xant_log')[23]),
     'Blend (BRTs & RF)': ('-', load_model('xant_blended')[23])
     }
 
 # load training (80%) and test (20%) sets
 
-env_data = pd.read_csv(
-    '/Users/danielfurman/Data_science_code/xantusia-codebase/data/envtrain_xv.csv')
+env_data = pd.read_csv('data/envtrain_xv.csv')
 training_class = env_data['pa']
 env_data = env_data.drop(['Unnamed: 0'], axis=1)
-training_data = env_data.drop(['pa'], axis=1) # drop class
+training_data = env_data.drop(['pa'], axis=1)
 
-env_data_test = pd.read_csv(
-    '/Users/danielfurman/Data_science_code/xantusia-codebase/data/envtest_xv.csv')
+env_data_test = pd.read_csv('data/envtest_xv.csv')
 validation_class = env_data_test['pa']
 env_data_test = env_data_test.drop(['Unnamed: 0'], axis=1)
-validation_data  = env_data_test.drop(['pa'], axis=1)
+validation_data = env_data_test.drop(['pa'], axis=1)
 
 # perform validation set analyses, iterate over dictionary :
 
-f_score = np.zeros(len(CLASS_MAP)) # initialize F score array
-col_names = [] # col_names for F score
-i = 0  # iterator
-style.use('default')
+f_score = np.zeros(len(CLASS_MAP))
+col_names = []
+i = 0
 style.use('ggplot')
-colors = ('tab:blue', 'tab:orange', 'tab:red', 'tab:grey', 'lightgreen', 
+colors = ('tab:blue', 'tab:orange', 'tab:red', 'tab:grey', 'lightgreen',
           'darkgoldenrod', 'black')
 plt.rcParams["figure.figsize"] = (6, 4)
 
@@ -70,10 +66,10 @@ for name, (line_fmt, model) in CLASS_MAP.items():
     preds = model.predict_proba(validation_data)
     pred = pd.Series(preds[:, 1])
     fpr, tpr, thresholds = roc_curve(validation_class, pred)
-    auc_score = auc(fpr, tpr) 
+    auc_score = auc(fpr, tpr)
     label = '%s: auc=%.4f' % (name, auc_score)
-    plt.plot(fpr, tpr, line_fmt,
-             linewidth=1.75, label=label, color = colors[i])
+    plt.plot(fpr, tpr, line_fmt, linewidth=1.75, label=label,
+             color=colors[i])
     # compute confusion matrix and F1 stat
     predicted_class_type = model.predict(validation_data)
     print('\n\nFraction correct validation ' + name + ' :',
@@ -87,10 +83,10 @@ for name, (line_fmt, model) in CLASS_MAP.items():
     col_names.append(name)
     i = + (i + 1)
 
-# annotate AUC Plot     
+# annotate AUC Plot
 plt.legend(loc="lower right", shadow=True)
-#plt.title('Comparing Classifiers: Validation AUC')
-plt.plot([0, 1], [0, 1], 'k-', alpha = 0.2) #x=y line. 
+# plt.title('Comparing Classifiers: Validation AUC')
+plt.plot([0, 1], [0, 1], 'k-', alpha=0.2)
 plt.ylim([0.0, 1.05])
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
